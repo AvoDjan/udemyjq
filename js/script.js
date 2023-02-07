cbd173c0de0245542d544cdc71d74589f6a8f32b
AvoDjan <119239831+AvoDjan@users.noreply.github.com>
2023-02-07 18:32:54 +0100

    Cours Udemy jQuery (jQuery & CSS) + modif HTML/CSS

--- js/script.js
+++ js/script.js
@@ -1,77 +1,74 @@
-// 74. Les bases de jQuery : jQuery & HTML
-
-
- $(function(){
-
-    var myVar = $("#p1").html();
-    console.log(myVar);
-
-// marque le code html
-    
- });
-
+// 75. Les bases de jQuery : jQuery & CSS
 
 
 
  $(function(){
 
-     $("#start").click(function(){
-        var myVar = $("#myInput").val();
-        console.log(myVar);
-     });
+    $("#but1").click(function(){
+        $("#p1").addClass("red");
+            // permet que le p1 soit ajouté dans la classe .red (css)
 
-    // Marque ce qu'on a écrit dans le input 
- });
+            // toggleClass : enlève ou remet la couleur rouge, ça dépend de comment il était
+
+
+
+    $("#p1").addClass("red bold");
+
+        // permet d'ajouter sur le P1 la classe red et bold (css)
+
+
+
+});
+
+
+
+
+    $("#but2").click(function(){
+        $("#p1").removeClass("red");
+
+        // retire la couleur rouge quand tu cliques sur le button 2
+    });
+
+
+
+
+});
+
+
+
 
 
 $(function(){
 
-    $("#start").click(function(){
-        var myVar = $("div").attr("style");
-        console.log(myVar);
-        // background : red
+    $("#but1").click(function(){
 
+        $("#p1").css({
+            "color": "red",
+            "font-weight": "bold",
+            "background": "yellow"
+        });
 
-
-        // $("#p1").text("Voilà mon nouveau texte");
-
-            // remplace "mon paragraphe 1" par le text "voilà mon nouveau texte" faut appuyer sur start pour faire ça
-
-
-
-        // $("#myInput").val("Nouvelle valeur");
-            // change ce que j'écris en mettant "Nouvelle valeur" quand j'appuis sur start
-
-
-
-        // $("div").append("<p>Mon nouveau paragraphe</p>");
-            // Ajout "<p>Mon nouveau paragraphe</p>" dans le html sans y rentrer à l'intérieur (il se positionne à la fin)
-
-            
-
-        // $("div").prepend("<p>Mon nouveau paragraphe</p>");
-            // Ajout "<p>Mon nouveau paragraphe</p>" dans le html sans y rentrer à l'intérieur (il se positionne au début)
-
-
-
-        // $("div").after("<p>Mon nouveau paragraphe</p>");
-                // Ajout "<p>Mon nouveau paragraphe</p>" dans le html sans y rentrer à l'intérieur (il se positionne hors du background red en bas)
-
-
-
-        // $("div").before("<p>Mon nouveau paragraphe</p>");
-            // Ajout "<p>Mon nouveau paragraphe</p>" dans le html sans y rentrer à l'intérieur (il se positionne hors du background red en haut)
-
-
-
-        $("#p1").remove();
-            // Retire l'élément et son contenu du html quand tu appuis sur start
-
-
-
-        // $("div").empty();
-            // Vide tout ce qu'il y a sur la page (tout le html et le background red)
+        // Change le code css via jQuery
     });
 
+
+});
+
+
+
+
+
+
+$(function(){
+    $("#but1").click(function(){
+
+        console.log($("#but1").width());
+
+            // donne la largeur du button 1
+
+        $("#but2").width(150);
+
+            // change la largeur du button2
+    });
 
 });
